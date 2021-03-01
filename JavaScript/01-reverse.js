774f058cb83caa5abbe3b710010005ad50b0d949
YounglanHong <yong2401@gmail.com>
2021-03-01 15:30:55 +0900

    Day01-reverse

---
+++ JavaScript/01-reverse.js
@@ -0,0 +1,18 @@
+// #01-reverse
+// accepts a string, returns a new string in reverse
+
+function reverse(str) {
+  let result = "";
+
+  function helper(helperStr) {
+    let str_length = helperStr.length;
+    if (helperStr.length === 0) return; // base case
+    result += helperStr.slice(-1); // store last char
+    helper(helperStr.slice(0, str_length - 1)); // recur
+  }
+  helper(str); // calls helper function
+
+  return result;
+}
+
+// reverse("string") // "gnirts"
