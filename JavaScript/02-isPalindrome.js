2ee4507a43fbedab0635a89484803c65292df1c2
YounglanHong <yong2401@gmail.com>
2021-03-02 11:21:55 +0900

    Day02-isPalindrome

--- JavaScript/02-isPalindrome.js
+++ JavaScript/02-isPalindrome.js
@@ -16,3 +16,12 @@ function isPalindrome(str) {
   helper(str);
   return result.every((el) => el); // when every elements is true, return true
 }
+
+/* 
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+*/
