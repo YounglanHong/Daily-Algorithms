913676a22c4264943a2e8c27e27c980cd7dd55b0
YounglanHong <yong2401@gmail.com>
2021-07-02 18:10:37 +0900

    Update Codility Algorithm: perm missing elem

--- Codility/3.2_permMissingElem.js
+++ Codility/3.2_permMissingElem.js
@@ -4,8 +4,11 @@
 // https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
 
 // A contains integers in the range [1..(N + 1)], 
-// which means that exactly one element is missing
+// which means that "exactly one element is missing"
+// 1) Sum of numbers between 1 and (N + 1)
+// 2) 1) - Accumulate elements in an array === missing element
 
+// Correctness failed
 function solution(A) {
   // N within the range [0, ...100,000]
   if(A.length === 0) return 1;
@@ -23,6 +26,20 @@ function solution(A) {
 }
 
 
+// Time Complexity: O(N) or O(N * log(N))
+function solution(A) {
+  const N = A.length;
+
+  // empty array
+  if(N === 0) return 1;
+  const permSum = ((N+1) * (N+2)) / 2;
+  const arraySum = A.reduce((acc, curr) => acc + curr);
+  const missingEl = permSum - arraySum;
+
+  return missingEl;
+}
+
+
 /* 
 describe("Find missing element in a given permutation"), () => {
   test("given array is []"), () => {
@@ -35,6 +52,14 @@ describe("Find missing element in a given permutation"), () => {
 
   test("given array is [2]"), () => {
     expect([2]).toBe(1)
+  })
+
+  test("given array is [2, 3]"), () => {
+    expect([2, 3]).toBe(1)
+  })
+
+  test("given array is [1, 2, 3]"), () => {
+    expect([1, 2, 3]).toBe(4)
   })
 }
 */
