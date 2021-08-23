fec71e31c146c28046a5ca2e5656f3e9f9807bff
YounglanHong <yong2401@gmail.com>
2021-08-23 20:45:32 +0900

    Add Programmers Algorithm: 약수의 합

--- Programmers/Level1/약수의합.js
+++ Programmers/Level1/약수의합.js
@@ -13,12 +13,15 @@ function solution(n) {
 
 // solution #2
 // 제곱근
+// 
 function solution(n) {
-  let total = [];
-  for(let i = 1; i <= Math.sqrt(n); i++) {
+  let sum = 0;
+  if(n === 1) return 1;
+    
+  for(let i = 0, j = Math.sqrt(n); i < j; i++) {
     if(n % i === 0) {
-      total.push(i, n / i); // pair: (3, 4), (2, 6), (1, 12)
+      sum += i + (n / i); // pair: (3, 4), (2, 6), (1, 12)
     }
   }
-  return total.reduce((acc, curr) => acc + curr);
+  return sum;
 }
